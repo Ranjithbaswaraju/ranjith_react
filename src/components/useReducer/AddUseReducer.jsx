983175141c326985db743ada82cb0e07d2ca6652
Ranjithbaswaraj <ranjithbaswaraju10@gmail.com>
2025-08-13 19:10:40 +0530

    updated

--- src/components/useReducer/AddUseReducer.jsx
+++ src/components/useReducer/AddUseReducer.jsx
@@ -1,64 +1,118 @@
-import React, { useReducer, useState } from "react";
+// import React, { useReducer, useState } from "react";
+// const reducer = (state, action) => {
+//   switch (action.type) {
+//     case "ADD_TODO":
+//       return {
+//         ...state,
+//         todos: [...state.todos, action.payload],
+//       };
+//     case "REMOVE_TODO":
+//       return {
+//         ...state,
+//         todos: state.todos.filter((_, index) => index != action.payload),
+//       };
+//   }
+// }
+// const AddUseReducer = () => {
+//   const intialState = {
+//     todos: [],
+//   }
+//   const [currentSate, dispatch] = useReducer(reducer, intialState);
+//   const [name, setName] = useState("");
+
+//   const enteredHandler = (e) => {
+//     setName(e.target.value);
+//   }
+//   const handler = () => {
+//     dispatch({
+//       type: "ADD_TODO",
+//       payload: name,
+//     })
+//     setName("");
+//   }
+//   const remove = (index) => {
+//     dispatch({
+//       type: "REMOVE_TODO",
+//       payload: index,
+//     })
+//   }
+//   return (
+//     <>
+//       <input type="text" onChange={enteredHandler} value={name} />
+//       <button onClick={handler}>Add Name</button>
+//       <ol>
+//         {currentSate.todos.map((eachtodo, index) => {
+//           return (
+//             <>
+//               <li>{eachtodo}</li>
+//               <button onClick={() => remove(index)}>Remove</button>
+//             </>
+//           );
+//         })}
+//       </ol>
+//     </>
+//   );
+// };
+// export default AddUseReducer;
+
+
+import React, { act, useReducer, useState } from "react";
+
 
 const reducer=(state,action)=>{
     switch(action.type){
-        case "ADD_TODO":
-            return{
-                ...state,
-                todos:[...state.todos,action.payload]
-            }
-        case "REMOVE_TODO":
-            return{
-                ...state,
-                todos:state.todos.filter((_,index)=>index!=action.payload)            }
+        case "ADD":
+            return {...state,todos:[...state.todos,action.payload]}
+        case "REMOVE":
+            return {...state,todos:state.todos.filter((_,index)=>index!==action.payload)}
     }
 }
 
-const AddUseReducer = () => {
-    const intialState={
+const AddUseReducer=()=>{
+    const initialState={
         todos:[]
     }
 
-    const[currentSate,dispatch]=useReducer(reducer,intialState)
     const[name,setName]=useState("")
 
-    const enteredHandler=(e)=>{
+    const[currentSate,dispatch]=useReducer(reducer,initialState)
+
+    const ranjith=(e)=>{
         setName(e.target.value)
     }
 
-    const handler=()=>{
+    const baswaraju=(e)=>{
+        e.preventDefault()
         dispatch({
-            type:'ADD_TODO',
+            type:"ADD",
             payload:name
         })
-        setName('')
     }
 
     const remove=(index)=>{
         dispatch({
-            type:"REMOVE_TODO",
+            type:'REMOVE',
             payload:index
         })
     }
-    return (
+
+    return(
         <>
-        <input type="text" onChange={enteredHandler} value={name}/>
-        <button onClick={handler}>Add Name</button>
-        <ol>
-            {
-            currentSate.todos.map((eachtodo,index)=>{
+        <input type='text' value={name} onChange={ranjith}/>
+        <button onClick={baswaraju}>Add_Todo</button>
+        {
+            currentSate.todos.map((item,index)=>{
                 return(
                     <>
-
-                    <li>{eachtodo}</li>
-                    <button onClick={()=>remove(index)}>Remove</button>
+                    <li>{item}</li>
+                    <button onClick={()=>remove(index)}>DELETE</button>
                     </>
                 )
             })
         }
-        </ol>
+        
+        
         </>
     )
 }
-
-export default AddUseReducer
+export default AddUseReducer;
