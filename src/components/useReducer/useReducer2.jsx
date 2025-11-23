7b7626ae7a8bd72f6b93e0f05ad9341c2a5664a1
Ranjithbaswaraj <ranjithbaswaraju10@gmail.com>
2025-11-23 15:17:58 +0530

    reducer

--- src/components/useReducer/useReducer2.jsx
+++ src/components/useReducer/useReducer2.jsx
@@ -53,3 +53,40 @@
 // export default UseReducer2
 
 
+import React, { useReducer, useState } from "react";
+const reducer=(state,action)=>{
+    switch(action.type){
+        case 'CHANGENAME':
+            return{...state,name:action.payload}
+    }
+}
+const UseReducer2 = () => {
+    const intialState={
+        name:'Ranjith',
+        count:0
+    }
+    const[currentState,dispatch]=useReducer(reducer,intialState)
+    const[name,setName]=useState('')
+
+    const handler=(e)=>{
+        setName(e.target.value)
+    }
+
+    const onSumbit=()=>{
+        dispatch({
+            type:"CHANGENAME",
+            payload:name
+        })
+        setName('')
+    }
+
+    return(
+        <>
+        <p>Your entered name is :{currentState.name}</p>
+        <input type="text" onChange={handler} value={name}/>
+        <button onClick={onSumbit}>Change Name</button>
+
+        </>
+    )
+};
+export default UseReducer2;
