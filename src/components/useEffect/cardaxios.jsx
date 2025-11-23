53341652eacbbe6d016ee42bc1e36b68da5a6f71
Ranjithbaswaraj <ranjithbaswaraju10@gmail.com>
2025-11-23 11:03:52 +0530

    updated

--- src/components/useEffect/cardaxios.jsx
+++ src/components/useEffect/cardaxios.jsx
@@ -1,34 +1,79 @@
+// import axios from "axios";
+// import React, { useState } from "react";
+// import BasicExample from "../useState/cardComponent";
+
+
+// const CardFake=()=>{
+//     const[data,setData]=useState([])
+
+//     const FetchData=async()=>{
+//         const finalData=await axios.get("https://fakestoreapi.com/products")
+//         setData(finalData.data)
+//         console.log(finalData.data)
+//     }
+//     FetchData()
+
+//     return(
+//         <>
+//         <h1 style={{textAlign:'center'}}>FakeStore Products</h1>
+//         <div style={{display:'flex',flexWrap:'wrap',gap:'20px',justifyContent:'center'}}>
+            
+//         {
+//             data.map((item)=>{
+                
+//                 return(
+//                     <>
+//                     <BasicExample title={item.title}
+//                     src={item.image}
+//                     price={item.price}
+//                     category={item.category}
+//                     description={item.description}
+                    
+                    
+//                     />
+//                     </>
+//                 )
+//             })
+//         }
+//         </div>
+//         </>
+//     )
+// }
+// export default CardFake
+
+
+
 import axios from "axios";
 import React, { useState } from "react";
-import BasicExample from "../useState/cardComponent";
-
-
-const CardFake=()=>{
+import BasicExample from '../useState/cardComponent'
+const  CardFake=()=>{
     const[data,setData]=useState([])
 
     const FetchData=async()=>{
-        const finalData=await axios.get("https://fakestoreapi.com/products")
-        setData(finalData.data)
-        console.log(finalData.data)
+        try{
+            const finalData=await axios.get("https://fakestoreapi.com/products")
+            console.log(finalData.data)
+            setData(finalData.data)
+        }
+        catch(err){
+            console.log(err)
+        }
     }
     FetchData()
 
     return(
+
         <>
-        <h1 style={{textAlign:'center'}}>FakeStore Products</h1>
-        <div style={{display:'flex',flexWrap:'wrap',gap:'20px',justifyContent:'center'}}>
-            
-        {
+        <div style={{display:'flex',flexWrap:'wrap',justifyContent:'center',gap:'20px'}}>
+            {
             data.map((item)=>{
-                
                 return(
                     <>
-                    <BasicExample title={item.title}
-                    src={item.image}
-                    price={item.price}
+                    <BasicExample
+                    title={item.title}
                     category={item.category}
+                    image={item.image}
                     description={item.description}
-                    
                     
                     />
                     </>
@@ -36,8 +81,8 @@ const CardFake=()=>{
             })
         }
         </div>
+        
         </>
     )
 }
-export default CardFake
-
+export default CardFake;
--- src/components/useState/cardComponent.jsx
+++ src/components/useState/cardComponent.jsx
@@ -1,40 +1,76 @@
+// import Button from 'react-bootstrap/Button';
+// import Card from 'react-bootstrap/Card';
+
+// function BasicExample({title,price,description,category,src}) {
+//   return (
+//     <Card style={{ width: '300px', height:'450px',border:'2px solid black' ,textAlign:'center'}}>
+//       <Card.Title ><h3 style={{
+//     display: "-webkit-box",
+//     WebkitBoxOrient: "vertical",
+//     WebkitLineClamp: 1,
+//     overflow: "hidden",
+//     height:"40px",
+//     textOverflow: "ellipsis"
+//   }}>{title}</h3></Card.Title>
+//       <Card.Img variant="top" src={src} width="200px" height="200px"/>
+//       <Card.Body>
+        
+//         <Card.Text>
+//           {`Price : ${price}`}
+//         </Card.Text>
+//         <Card.Text>
+//           {`Category : ${category}`}
+//         </Card.Text>
+//          <Card.Text  style={{
+//     display: "-webkit-box",
+//     WebkitBoxOrient: "vertical",
+//     WebkitLineClamp: 3,
+//     overflow: "hidden",
+//     textOverflow: "ellipsis"
+//   }}>
+//           {description}
+//         </Card.Text>
+//         <Button variant="primary">Buy Now</Button>
+//       </Card.Body>
+//     </Card>
+//   );
+// }
+
+// export default BasicExample;
+
+
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
-function BasicExample({title,price,description,category,src}) {
+function BasicExample({title,category,image,description}) {
   return (
-    <Card style={{ width: '300px', height:'450px',border:'2px solid black' ,textAlign:'center'}}>
-      <Card.Title ><h3 style={{
-    display: "-webkit-box",
-    WebkitBoxOrient: "vertical",
-    WebkitLineClamp: 1,
-    overflow: "hidden",
-    height:"40px",
-    textOverflow: "ellipsis"
-  }}>{title}</h3></Card.Title>
-      <Card.Img variant="top" src={src} width="200px" height="200px"/>
+    <Card style={{ width: '300px' ,height:'500px',border:'2px solid red',textAlign:'center'}}>
+      
       <Card.Body>
-        
-        <Card.Text>
-          {`Price : ${price}`}
-        </Card.Text>
-        <Card.Text>
-          {`Category : ${category}`}
-        </Card.Text>
-         <Card.Text  style={{
-    display: "-webkit-box",
-    WebkitBoxOrient: "vertical",
-    WebkitLineClamp: 3,
-    overflow: "hidden",
-    textOverflow: "ellipsis"
-  }}>
+        <Card.Title ><h3
+         style={{
+            display: '-webkit-box',
+            WebkitBoxOrient: 'vertical',
+            WebkitLineClamp: 2,
+            overflow: 'hidden',
+            textOverflow: 'ellipsis',
+            height:"40px"
+        }}>{title}</h3></Card.Title>
+        <Card.Subtitle><h4>{`Category : ${category}`}</h4></Card.Subtitle>
+        <Card.Img  src={image} width="200px" height="200px" />
+        <Card.Text style={{
+          display:'-webkit-box',
+          webkitBoxOrient:'vertical',
+          WebkitLineClamp:3,
+          overflow:'hidden',
+          textOverflow:'ellipsis'
+        }}>
           {description}
         </Card.Text>
-        <Button variant="primary">Buy Now</Button>
+        <Button variant="primary">Go somewhere</Button>
       </Card.Body>
     </Card>
   );
 }
 
 export default BasicExample;
-
