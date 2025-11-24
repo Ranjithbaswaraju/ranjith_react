31fafcdbaae418d539ef5ecdb87b3e6df4a8e9ad
Ranjithbaswaraj <ranjithbaswaraju10@gmail.com>
2025-11-24 16:49:50 +0530

    updated

--- src/components/contextapi/contextapi.jsx
+++ src/components/contextapi/contextapi.jsx
@@ -1,23 +1,49 @@
+// import React, { createContext, useContext } from "react";
+
+// const messageContext=createContext();
+// const ContextApi=()=>{
+//     const message='Hello World I am ContextApi'
+//     return(
+//         <messageContext.Provider value={message}>
+//             <h1>ContextApi</h1>
+//             <ChildComponent/>
+//         </messageContext.Provider>
+//     )
+// }
+
+// const ChildComponent=()=>{
+//     let message1=useContext(messageContext);
+//     return(
+//         <>
+//         <h2>ChildComponent</h2>
+//         <h4>{message1}</h4>
+//         </>
+//     )
+// }
+// export default ContextApi;
+
+
 import React, { createContext, useContext } from "react";
+const MessageContext=createContext()
 
-const messageContext=createContext();
 const ContextApi=()=>{
-    const message='Hello World I am ContextApi'
-    return(
-        <messageContext.Provider value={message}>
-            <h1>ContextApi</h1>
-            <ChildComponent/>
-        </messageContext.Provider>
-    )
-}
+    let message='Hello Ranjith'
 
-const ChildComponent=()=>{
-    let message1=useContext(messageContext);
     return(
         <>
-        <h2>ChildComponent</h2>
-        <h4>{message1}</h4>
+        <MessageContext.Provider value={message}>
+            <h1>ContextApi</h1>
+            <ChildComponent/>
+        </MessageContext.Provider>
         </>
     )
 }
-export default ContextApi;
+const ChildComponent=()=>{
+    let ranjith=useContext(MessageContext)
+    return(
+        <>
+        <p>{ranjith}</p>
+        </>
+    )
+}
+export default ContextApi
