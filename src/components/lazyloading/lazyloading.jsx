dab56136f2c721b23d537508d4f60b3614ea9d06
Ranjithbaswaraj <ranjithbaswaraju10@gmail.com>
2025-12-19 10:37:09 +0530

    updated

--- src/components/lazyloading/lazyloading.jsx
+++ src/components/lazyloading/lazyloading.jsx
@@ -1,25 +1,51 @@
-import React, { Suspense } from "react";
-import { lazy } from "react";
+// import React, { Suspense } from "react";
+// import { lazy } from "react";
 
+// const Component1=lazy(()=>
+//     import("../contextapi/contextapi")
+// )
+// const Component2=lazy(()=>
+//     import("../useEffect/fetch")
+// )
 
-const Component1=lazy(()=>
-    import("../contextapi/contextapi")
-)
-const Component2=lazy(()=>
-    import("../useEffect/fetch")
-)
+// const Ranjith=()=>{
+//     return(
+//         <>
+//         <h1>Lazy Loading....</h1>
+//         <Suspense fallback={<h1>Loading...</h1>}>
+//             <Component2/>
+//         </Suspense>
+//         <Suspense fallback={<h4>Loading...</h4>}>
+//             <Component1/>
+//         </Suspense>
+//         </>
+//     )
+// }
+// export default Ranjith;
 
-const Ranjith=()=>{
-    return(
-        <>
-        <h1>Lazy Loading....</h1>
-        <Suspense fallback={<h1>Loading...</h1>}>
-            <Component2/>
-        </Suspense>
-        <Suspense fallback={<h4>Loading...</h4>}>
-            <Component1/>
-        </Suspense>
-        </>
-    )
-}
+import React, { lazy, Suspense } from "react";
+
+const Component1 = lazy(() => 
+  import("../contextapi/contextapi")
+);
+
+const Component2 = lazy(() => 
+  import("../useEffect/axios")
+);
+
+const Ranjith = () => {
+  return (
+    <>
+      <h1>Lazy loading.......</h1>
+
+      <Suspense fallback={<h1>loading.....</h1>}>
+        <Component1 />
+      </Suspense>
+
+      <Suspense fallback={<h1>loading....</h1>}>
+        <Component2 />
+      </Suspense>
+    </>
+  );
+};
 export default Ranjith;
