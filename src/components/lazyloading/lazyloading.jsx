5639282f5447b0f0b03484e4fcb35ed348cd6b45
Ranjithbaswaraj <ranjithbaswaraju10@gmail.com>
2026-04-30 16:19:15 +0530

    m

--- src/components/lazyloading/lazyloading.jsx
+++ src/components/lazyloading/lazyloading.jsx
@@ -23,29 +23,55 @@
 // }
 // export default Ranjith;
 
+// import React, { lazy, Suspense } from "react";
+
+// const Component1 = lazy(() => 
+//   import("../contextapi/contextapi")
+// );
+
+// const Component2 = lazy(() => 
+//   import("../useEffect/axios")
+// );
+
+// const Ranjith = () => {
+//   return (
+//     <>
+//       <h1>Lazy loading.......</h1>
+
+//       <Suspense fallback={<h1>loading.....</h1>}>
+//         <Component1 />
+//       </Suspense>
+
+//       <Suspense fallback={<h1>loading....</h1>}>
+//         <Component2 />
+//       </Suspense>
+//     </>
+//   );
+// };
+// export default Ranjith;
+
+
+
 import React, { lazy, Suspense } from "react";
 
-const Component1 = lazy(() => 
-  import("../contextapi/contextapi")
-);
+const Component1=lazy(()=>
+    import("../useEffect/axios")
+  )
+  const Component2=lazy(()=>
+    import("../contextapi/contextapi")
+  )
+const Ranjith=()=>{
 
-const Component2 = lazy(() => 
-  import("../useEffect/axios")
-);
-
-const Ranjith = () => {
-  return (
+  return(
     <>
-      <h1>Lazy loading.......</h1>
+    <Suspense fallback={<h1>Loading......</h1>}>
+      <Component1/>
+    </Suspense>
 
-      <Suspense fallback={<h1>loading.....</h1>}>
-        <Component1 />
-      </Suspense>
-
-      <Suspense fallback={<h1>loading....</h1>}>
-        <Component2 />
-      </Suspense>
+    <Suspense fallback={<h2>Please wait the page is loading....</h2>}>
+      <Component2/>
+    </Suspense>
     </>
-  );
-};
+  )
+}
 export default Ranjith;
