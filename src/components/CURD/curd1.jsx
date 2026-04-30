b0f8e0147eb35263f37fd3890783a36eb8bc509b
Ranjithbaswaraj <ranjithbaswaraju10@gmail.com>
2026-04-30 15:42:45 +0530

    m

--- src/components/CURD/curd1.jsx
+++ src/components/CURD/curd1.jsx
@@ -1,94 +1,48 @@
-
-
-
-
-// import React, { useState } from "react";
-// const CurdAddDel = () => {
-//   const[name,setName]=useState('')
-//   const[todos,setTodos]=useState([])
-//   const handler=(e)=>{
-//     setName(e.target.value)
-//   }
-
-//   const ranjith=(e)=>{
-//     e.preventDefault()
-//     if(name){
-//       setTodos([...todos,name])
-    
-//     }
-//     setName('')
-//   }
-//   const remove=(ind)=>{
-//     const updatedTodo=todos.filter((item,index)=>index!=ind)
-//     setTodos(updatedTodo)
-//   }
-//     return(
-//       <>
-//       <form onSubmit={ranjith}>
-//         <input value={name} onChange={handler}/>
-//         <button type="submit">Add</button>
-//         <ol>
-//           {
-//           todos?.map((item,index)=>{
-//             return(
-//               <>
-//               <li>{item}</li>
-//               <button onClick={()=>remove(index)}>Delete Todo</button>
-//               </>
-//             )
-//           })
-//           }
-//           </ol>
-//       </form>
-//       </>
-//     )
-// }
-// export default CurdAddDel;
-
-
-
 import React, { useState } from "react";
-
 
 const CurdAddDel=()=>{
   const[name,setName]=useState("")
   const[todos,setTodos]=useState([])
 
-const ranjith=(e)=>{
-  e.preventDefault()
-  if(name){
-    setTodos([...todos,name])
+  const ranjith=(e)=>{
+    e.preventDefault()
+
+    if(name){
+      setTodos([...todos,name])
+      setName("")
+    }
+    else{
+      setName("")
+    }
   }
-  setName("")
 
-
-}
-
-const deleteTodo=(ind)=>{
-  const updateTodo=todos.filter((_,index)=>index!=ind)
-  setTodos(updateTodo)
-}
+  const deleteHandler=(ind)=>{
+    const updatedTodo=todos.filter((_,index)=>index!=ind)
+    setTodos(updatedTodo)
+  }
 
   return(
     <>
+   
     <form onSubmit={ranjith}>
-    <input type="text" value={name} onChange={(e)=>setName(e.target.value)}/>
-    <button type="submit">Add Todo</button>
-    {
-      todos?.map((item,index)=>{
-        return(
-          <>
-          <p>{item}</p>
-          <button onClick={()=>deleteTodo(index)}>Delete</button>
-          </>
-        )
-      })
-    }
 
+      <input type="text" value={name} onChange={(e)=>setName(e.target.value)}/>
+      <button type="submit">Add Todo</button>
+      <ol>
+      {
+        todos?.map((item,index)=>{
+          return(
+            <>
+            <li>{item}</li>
+            <button onClick={()=>deleteHandler(index)}>Delete</button>
+            </>
+          )
+        })
+      }
+      </ol>
     </form>
     
     </>
   )
 }
-
-export default CurdAddDel;
+export default CurdAddDel
