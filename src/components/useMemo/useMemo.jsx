7741b709607cb588f8a1d3abb682554997cebb24
Ranjithbaswaraj <ranjithbaswaraju10@gmail.com>
2026-06-24 21:30:03 +0530

    updated

--- src/components/useMemo/useMemo.jsx
+++ src/components/useMemo/useMemo.jsx
@@ -1,76 +1,3 @@
-// import React, { useMemo, useState } from "react";
-
-
-// const UseMemoExample=()=>{
-//     const[riceQty,setRiceQty]=useState(0)
-//     const[milkQty,setMilkQty]=useState(0)
-
-//     const milkPriceCalculation=useMemo(()=>{
-//         console.log("<<<<the milk calucaltion>>>")
-
-//         const price=50
-//         return price*milkQty
-//     },[milkQty])
-
-//     const ricePriceCalculation=useMemo(()=>{
-//         console.log("<<<<the rice calucaltion>>>")
-//         const price=100
-//         return price*riceQty
-//     },[riceQty])
-
-//     const riceHandler=()=>{
-//         setRiceQty(riceQty+1)
-//     }
-//     const milkHandler=()=>{
-//         setMilkQty(milkQty+1)
-//     }
-
-//     return(
-//         <>
-//         <p>riceQty:{riceQty}-- price-{ricePriceCalculation}</p>
-//         <p>milkQty:{milkQty} -- price-{milkPriceCalculation}</p>
-//         <button onClick={riceHandler}>Rice</button>
-//         <button onClick={milkHandler}>Milk</button>
-//         </>
-//     )
-// }
-// export default UseMemoExample
-
-
-// const UseMemoExample=()=>{
-//     const[riceQty,setRiceQty]=useState(0)
-//     const[milkQty,setMilkQty]=useState(0)
-
-//     const RiceHandler=()=>{
-//         setRiceQty(riceQty+1)
-//     }
-//     const MilkHandler=()=>{
-//         setMilkQty(milkQty+1)
-//     }
-
-//     const ricePriceCalculation=useMemo(()=>{
-//         console.log('<<<Rice price calculation>>>')
-//         const riceprice=50
-//         return riceprice*riceQty
-//     },[riceQty])
-
-//     const milkPriceCalculation=useMemo(()=>{
-//         console.log('<<<Milk price calculation>>>')
-//         const milkprice=100
-//         return milkprice*milkQty
-//     },[milkQty])  
-//     return(
-//         <>
-//         <p>Milk Qty --{milkQty} --price --{milkPriceCalculation}</p>
-//         <p>Rice Qty --{riceQty} --price --{ricePriceCalculation}</p>
-//         <button onClick={MilkHandler}>MILK</button>
-//         <button onClick={RiceHandler}>RICE</button>
-//         </>
-//     )
-// }
-// export default UseMemoExample;
-
-
 import React,{useState,useMemo} from "react";
 
 const UseMemoExample=()=>{
@@ -107,3 +34,5 @@ return(<>
 </>)
 }
 export default UseMemoExample
+
+
