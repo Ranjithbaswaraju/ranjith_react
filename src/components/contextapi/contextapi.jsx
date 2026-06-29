8a27151e171e0bbdfb3e9ccfd9b24349be24d74a
Ranjithbaswaraj <ranjithbaswaraju10@gmail.com>
2026-06-29 11:34:45 +0530

    updated

--- src/components/contextapi/contextapi.jsx
+++ src/components/contextapi/contextapi.jsx
@@ -1,26 +1,4 @@
-// import React, { createContext, useContext } from "react";
 
-// const messageContext=createContext();
-// const ContextApi=()=>{
-//     const message='Hello World I am ContextApi'
-//     return(
-//         <messageContext.Provider value={message}>
-//             <h1>ContextApi</h1>
-//             <ChildComponent/>
-//         </messageContext.Provider>
-//     )
-// }
-
-// const ChildComponent=()=>{
-//     let message1=useContext(messageContext);
-//     return(
-//         <>
-//         <h2>ChildComponent</h2>
-//         <h4>{message1}</h4>
-//         </>
-//     )
-// }
-// export default ContextApi;
 
 
 import React, { createContext, useContext } from "react";
--- src/components/lazyloading/lazyloading.jsx
+++ src/components/lazyloading/lazyloading.jsx
@@ -1,76 +1,17 @@
-// import React, { Suspense } from "react";
-// import { lazy } from "react";
-
-// const Component1=lazy(()=>
-//     import("../contextapi/contextapi")
-// )
-// const Component2=lazy(()=>
-//     import("../useEffect/fetch")
-// )
-
-// const Ranjith=()=>{
-//     return(
-//         <>
-//         <h1>Lazy Loading....</h1>
-//         <Suspense fallback={<h1>Loading...</h1>}>
-//             <Component2/>
-//         </Suspense>
-//         <Suspense fallback={<h4>Loading...</h4>}>
-//             <Component1/>
-//         </Suspense>
-//         </>
-//     )
-// }
-// export default Ranjith;
-
-// import React, { lazy, Suspense } from "react";
-
-// const Component1 = lazy(() => 
-//   import("../contextapi/contextapi")
-// );
-
-// const Component2 = lazy(() => 
-//   import("../useEffect/axios")
-// );
-
-// const Ranjith = () => {
-//   return (
-//     <>
-//       <h1>Lazy loading.......</h1>
-
-//       <Suspense fallback={<h1>loading.....</h1>}>
-//         <Component1 />
-//       </Suspense>
-
-//       <Suspense fallback={<h1>loading....</h1>}>
-//         <Component2 />
-//       </Suspense>
-//     </>
-//   );
-// };
-// export default Ranjith;
-
-
 
 import React, { lazy, Suspense } from "react";
-
 const Component1=lazy(()=>
-    import("../useEffect/axios")
-  )
-  const Component2=lazy(()=>
-    import("../contextapi/contextapi")
-  )
-const Ranjith=()=>{
+  import("../useEffect/fetch")
+)
+const Component2=lazy(()=>
+import("../contextapi/contextapi")
+)
 
+const Ranjith=()=>{
   return(
     <>
-    <Suspense fallback={<h1>Loading......</h1>}>
-      <Component1/>
-    </Suspense>
-
-    <Suspense fallback={<h2>Please wait the page is loading....</h2>}>
-      <Component2/>
-    </Suspense>
+    <Suspense fallback={<div>....Loading1</div>}><Component1/></Suspense>
+    <Suspense fallback={<div>...Loading2</div>}><Component2/></Suspense>
     </>
   )
 }
