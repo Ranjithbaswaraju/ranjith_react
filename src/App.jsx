68400fa63137c5d41b3b286428232619845bd8eb
Ranjithbaswaraj <ranjithbaswaraju10@gmail.com>
2026-06-29 14:34:38 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,5 @@
+import Timer from "./components/StopWatch";
 
-
-
-import React from "react";
-// import ProfileHidden from "./components/ProfileHidden";
-import Parent from "./components/formComponent";
-import KeysComponent from "./components/ProfileHidden";
-import Timer from "./components/ProfileHidden";
-import { Pagination } from "react-bootstrap";
 
 
 
@@ -14,11 +7,9 @@ const App=()=>{
   return(
 
     <>
-    {/* <ProfileHidden/> */}
-    {/* <Parent/> */}
-    {/* <KeysComponent/> */}
+  
     <Timer/>
-    {/* <Pagination/> */}
+    
     </>
   )
 }
---
+++ src/components/StopWatch.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+
+const Timer=()=>{
+    const[run,setRun]=useState(false)
+    const[time,setTime]=useState(0)
+
+    useEffect(()=>{
+        let intervalId;
+
+        if(run){
+            intervalId=setInterval(()=>{
+                setTime((prev=>prev+1))
+            },1000)
+        }
+        return ()=>clearInterval(intervalId)
+    },[run])
+
+    const hours=Math.floor(time/3600)
+    const minutes=Math.floor((time%3600)/60)
+    const seconds=time%60
+
+    return(
+        <>
+
+        <p>
+            {String(hours).padStart(2,"0")}:
+            {String(minutes).padStart(2,"0")}:
+            {String(seconds).padStart(2,"0")}
+
+        </p>
+        <button onClick={()=>setRun(true)}>Start</button>
+        <button onClick={()=>setRun(false)}>Stop</button>
+        <button onClick={()=>{
+            setRun(false)
+            setTime(0)
+        }}>Reset</button>
+        
+        </>
+    )
+
+
+}
+export default Timer;
